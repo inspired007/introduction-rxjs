182f6d548f15a2861ae79f8d22e4118b943c5452
Randy Ferrer <randfer@gmail.com>
2015-10-04 13:50:25 -0400

    Async Request/Response

---
+++ asyncrecres/recres.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var requestStream = Rx.Observable.just('https://api.github.com/users');
+
+var responseStream = requestStream
+  .flatMap(requestUrl =>
+    Rx.Observable.fromPromise(jQuery.getJSON(requestUrl))
+  );
+
+responseStream.subscribe(response => {
+  console.log(response);
+});
